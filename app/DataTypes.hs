c64b5536fe335e4cda06578ca14b3de7faad63db
Adam-Alani <adamalany@gmail.com>
2024-03-15 16:36:38 +0100

    split up files

---
+++ app/DataTypes.hs
@@ -0,0 +1,53 @@
+module DataTypes where
+
+import Data.Maybe (fromMaybe)
+
+data Request = Request
+  { method :: !String,
+    path :: !String,
+    headers :: ![(String, String)],
+    body :: !String
+  }
+  deriving (Show)
+
+data Response = Response
+  { status :: !StatusCode,
+    resp_body :: !(Maybe String)
+  }
+
+instance Show Response where
+  show r = "HTTP/1.1 " ++ show (status r) ++ "\r\n\r\n" ++ fromMaybe "" (resp_body r)
+
+data StatusCode = OK | NotFound | InternalServerError | UnknownStatusCode Int
+
+instance Show StatusCode where
+  show OK = "200 OK"
+  show NotFound = "404 Not Found"
+  show InternalServerError = "500 Internal Server Error"
+  show (UnknownStatusCode code) = show code ++ " Unknown Status Code"
+
+serialize :: String -> Maybe Request
+serialize requestStr = do
+  (headerLines, bodyLines) <- splitAtHeaderAndBody requestStr
+  let maybeHeaders = map parseHeader headerLines
+  case sequence maybeHeaders of
+    Nothing -> Nothing
+    Just parsedHeaders -> do
+      let headers' = parsedHeaders
+      let methodLine = head headerLines
+      let method' = takeWhile (/= ' ') methodLine
+      let pathLine = words methodLine !! 1
+      let path' = takeWhile (/= ' ') pathLine
+      return Request {method = method', path = path', headers = headers', body = unlines bodyLines}
+
+-- | Splits a request string into a header and a body.
+splitAtHeaderAndBody :: String -> Maybe ([String], [String])
+splitAtHeaderAndBody requestStr =
+  let (headerStr, bodyStr) = break (== "\r\n\r\n") (lines requestStr)
+   in Just (headerStr, drop 1 bodyStr)
+
+-- | Parses a header line into a key-value pair.
+parseHeader :: String -> Maybe (String, String)
+parseHeader line =
+  let (name, value) = break (== ':') line
+   in Just (name, dropWhile (== ' ') (drop 1 value))
---
+++ app/Handler.hs
@@ -0,0 +1,58 @@
+module Handler where
+
+import Control.Concurrent (forkIO)
+import Data.String (IsString (fromString))
+import DataTypes (Request (path), Response (Response, resp_body, status), StatusCode (InternalServerError, NotFound, OK), serialize)
+import Network.Socket
+import Network.Socket.ByteString (recv, sendAll)
+import System.Directory
+import System.IO
+
+pathRoot :: String
+pathRoot = "./server"
+
+runServer :: AddrInfo -> IO ()
+runServer serveraddr = do
+  sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+  bind sock (addrAddress serveraddr)
+  listen sock 5
+
+  putStrLn "Listening on port 3000"
+  acceptLoop sock
+
+acceptLoop :: Socket -> IO ()
+acceptLoop sock = do
+  (conn, _) <- accept sock
+  putStrLn "Accepted connection "
+  _ <- forkIO $ handleConn conn
+  acceptLoop sock
+
+handleConn :: Socket -> IO ()
+handleConn conn = do
+  req <- recv conn 1024
+
+  let request = serialize (show req)
+  case request of
+    Nothing -> do
+      sendAll conn (fromString (show Response {status = InternalServerError, resp_body = Nothing}))
+    Just req_data -> do
+      handleRequest req_data conn
+
+  close conn
+
+handleRequest :: Request -> Socket -> IO ()
+handleRequest req conn = do
+  let path' = pathRoot ++ path req
+
+  fileExists <- doesFileExist path'
+  if fileExists
+    then do
+      handle <- openFile path' ReadMode
+      contents <- hGetContents handle
+      print contents
+      let response = Response {status = OK, resp_body = Just contents}
+      sendAll conn (fromString (show response))
+    else do
+      let response = Response {status = NotFound, resp_body = Nothing}
+      sendAll conn (fromString (show response))
+      print $ show response
